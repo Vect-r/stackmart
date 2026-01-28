dad5cfa2a47e187e488b121f3adeda844d3c39ed
Mitesh <miteshvaid764@gmail.com>
2026-01-28 16:14:45 +0530

    Created Views, added designs.

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "dashboard/index.html")
+
+def login(request):
+    return render(request, "dashboard/login.html")
+
+def about(request):
+    return render(request, "dashboard/about.html")
+
+def register(request):
+    return render(request, "dashboard/create_user.html")
+
+def contact(request):
+    return render(request, "dashboard/contact.html")
+
+def forgot_password(request):
+    return render(request, "dashboard/forgot_password.html")
+
+def reset_password(request):
+    return render(request, "dashboard/reset_password.html")
+
+def blog(request):
+    return render(request, "dashboard/blog.html")
