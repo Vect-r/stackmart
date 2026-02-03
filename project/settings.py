6b1b15d7d3a7750c3d68be93b0e481d0444c0c16
Mitesh <miteshvaid764@gmail.com>
2026-02-03 10:07:12 +0530

    Updated Design, Bug Fixed, Correct responsive layout

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r7-bxqu1yndi%^xyib^id@4xj(2p))k(e0frky&%a*-%29e6u1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
